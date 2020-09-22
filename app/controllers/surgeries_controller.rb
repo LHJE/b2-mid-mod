789c94569421843aa82cb7e8f010072bf6d2a118
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-22 11:38:22 -0600

    Refactor surgeries controller

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -1,13 +1,12 @@
 class SurgeriesController < ApplicationController
   def index
     @surgeries = Surgery.all
-    @doctor_surgeries = DoctorSurgery.all
   end
 
   def show
     @surgery = Surgery.find(params[:id])
     @other_surgeries = Surgery.where(day: @surgery.day)
-    @doctor_surgeries = DoctorSurgery.where(surgery_id: @surgery.id)
+    @doctors = Doctor.where(id: DoctorSurgery.where(surgery_id: @surgery.id).pluck(:doctor_id)).pluck(:name)
   end
 
 end
