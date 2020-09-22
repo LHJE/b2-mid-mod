f073c432fece50448dc28667b24af8b4cb538ea1
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-22 11:18:39 -0600

    Update doctor surgery controller with hospital id conditional

--- app/controllers/doctor_surgeries_controller.rb
+++ app/controllers/doctor_surgeries_controller.rb
@@ -2,9 +2,10 @@ class DoctorSurgeriesController < ApplicationController
 
   def update
     @surgery = Surgery.find(params[:id])
-    @doctor_surgeries = DoctorSurgery.where(surgery_id: @surgery.id)
     @new_doctor = Doctor.where(name: params[:name]).reduce
-    DoctorSurgery.create!(surgery_id: @surgery.id, doctor_id: @new_doctor.id)
+    if @new_doctor.hospital_id == @surgery.hospital_id
+      DoctorSurgery.create!(surgery_id: @surgery.id, doctor_id: @new_doctor.id)
+    end
     redirect_to "/surgeries/#{@surgery.id}"
   end
 
