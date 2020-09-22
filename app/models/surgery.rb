9b5be7d00789f9eb2eba53fa0b336bc2dfa0141a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-22 09:17:38 -0600

    Add Surgery model

---
+++ app/models/surgery.rb
@@ -0,0 +1,7 @@
+class Surgery < ApplicationRecord
+  has_many :doctor_surgeries
+  has_many :doctors, through: :doctor_surgeries
+
+  validates_presence_of :title, :day, :operating_room_number
+
+end
