fa2e2fc6d4c08daae700629e4f44811ff49b1b5a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-22 10:54:52 -0600

    Update doctor surgeries controller

--- app/controllers/doctor_surgeries_controller.rb
+++ app/controllers/doctor_surgeries_controller.rb
@@ -1,7 +1,11 @@
 class DoctorSurgeriesController < ApplicationController
 
   def update
-
+    @surgery = Surgery.find(params[:id])
+    @doctor_surgeries = DoctorSurgery.where(surgery_id: @surgery.id)
+    @new_doctor = Doctor.where(name: params[:name]).reduce
+    DoctorSurgery.create!(surgery_id: @surgery.id, doctor_id: @new_doctor.id)
+    redirect_to "/surgeries/#{@surgery.id}"
   end
 
 end
